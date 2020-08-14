101d6609e691b7c21351a1419dada0d45dccfa00
Akash Srivastava <akashsrivastava@Akashs-MacBook-Pro.local>
2020-08-14 22:36:09 +0530

    adding .gitignore

--- Strategy Pattern/Main.java
+++ Strategy Pattern/Main.java
@@ -3,10 +3,7 @@ class Main{
         System.out.println("Testing");
 
         Birds parrot = new Birds("Parrot", 24);
-//        parrot.setFlyingBehaviour(new ItFly());
-
         Mammals whale = new Mammals("Whale", 500);
-        whale.setFlyingBehaviour(new ItCantFly());
 
         System.out.println(parrot);
         System.out.println(whale);
